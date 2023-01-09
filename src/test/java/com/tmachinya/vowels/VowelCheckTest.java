14ba88f64836785204d71c411d2130f56e9d51db
Tongesai <machinyat@gmail.com>
2023-01-09 16:46:27 +0200

    other test classes

---
+++ src/test/java/com/tmachinya/vowels/VowelCheckTest.java
@@ -0,0 +1,11 @@
+package com.tmachinya.vowels;
+
+import com.tmachinya.vowels.ifaces.VowelCheck;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class VowelCheckTest {
+}
